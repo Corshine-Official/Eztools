9278ef4916172505895071c4fc82ee68f4b92e18
Corshine-Official <r4divanhanief@gmail.com>
2020-05-02 23:53:57 +0700

    Edited README

--- corshine-macchanger.py
+++ corshine-macchanger.py
@@ -20,12 +20,17 @@ def new_mac():
     new_ += get_rand() + get_rand() 
     return new_
 
+
+#Changing eth0
+
 print(os.system("ifconfig eth0 | grep ether | grep -oE [0-9abcdef:]{17}")) 
 subprocess.call(["sudo","ifconfig","eth0","down"]) 
 new_m = new_mac() 
 subprocess.call(["sudo","ifconfig","eth0","hw","ether","%s"%new_m]) 
 subprocess.call(["sudo","ifconfig","eth0","up"]) 
 print(os.system("ifconfig eth0 | grep ether | grep -oE [0-9abcdef:]{17}"))
+
+#Chaning wlan0
 
 print(os.system("ifconfig wlan0 | grep ether | grep -oE [0-9abcdef:]{17}")) 
 subprocess.call(["sudo","ifconfig","wlan0","down"]) 
